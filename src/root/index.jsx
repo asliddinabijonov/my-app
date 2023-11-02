16f14753f718edf8932e03a22cb6055d73436e61
asliddinabijonov <asliddinanijonov04@gmail.com>
2023-11-02 15:12:21 +0500

    salom

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "../containers/App";
-import { Provider } from "react-redux";
-import store from "../redux/store";
+// import { Provider } from "react-redux";
+// import store from "../redux/store";
 
 const Root = () => {
   return (
     <Router>
-      <Provider store={store}>
+      {/* <Provider store={store}> */}
         <App />
-      </Provider>
+      {/* </Provider> */}
     </Router>
   );
 };
